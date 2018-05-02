789ae466418636777295a9502ca398c169a6b5da
Deise Claudino Almeida Pereira <deise.claudino@gmail.com>
2018-05-02 18:48:42 -0300

    Testando a segunda passagem por valor

--- java-codigos/sintaxe-variaveis-e-fluxo/src/TestaValores.java
+++ java-codigos/sintaxe-variaveis-e-fluxo/src/TestaValores.java
@@ -5,7 +5,8 @@ public class TestaValores {
 		int primeiro = 5;
 		int segundo = 7;
 		segundo = primeiro;
-		
+		primeiro = 10;
+		//copiamos 5 para dentro de segundo, e depois copiamos 10 para primeiro, a linha segundo = primeiro; não diz nada.
 		System.out.println(segundo);
 	}
 }
