88c48317d1f92a33afc070983c6c80659443fecf
Deise Claudino Almeida Pereira <deise.claudino@gmail.com>
2018-05-02 15:06:23 -0300

    Trabalhando com o tipo inteiro e variáveis

---
+++ java-codigos/sintaxe-variaveis-e-fluxo/src/TesteVariaveis.java
@@ -0,0 +1,21 @@
+
+public class TesteVariaveis {
+	public static void main(String[] args) {
+		System.out.println("Novo teste");
+		
+		int idade;
+		idade = 37;
+		
+		System.out.println(idade);
+		
+		System.out.println("A idade é" + idade);
+		
+		idade = 30 + 40;
+		System.out.println(idade);
+		
+		
+		idade = (3 * 4)/2;
+		System.out.println(idade);
+	}
+
+}
