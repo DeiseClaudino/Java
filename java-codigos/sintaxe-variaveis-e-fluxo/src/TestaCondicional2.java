f575373f2245c2fafa27dd67ead4b412d6a2e75c
Deise Claudino Almeida Pereira <deise.claudino@gmail.com>
2018-05-03 16:45:00 -0300

    Utilizando boolean na condicional com operador lógico &&

--- java-codigos/sintaxe-variaveis-e-fluxo/src/TestaCondicional2.java
+++ java-codigos/sintaxe-variaveis-e-fluxo/src/TestaCondicional2.java
@@ -1,10 +1,13 @@
 
 public class TestaCondicional2 {
 	public static void main(String[] args) {
-		System.out.println("Testando Condicionais");
-		int idade = 16;
-		int quantidadePessoas = 3;
-		if(idade >= 18 || quantidadePessoas >= 2) {
+		System.out.println("Testando Condicionais com operadores lógicos");
+		int idade = 19;
+		boolean acompanhado = true;
+		// OU   int quantidadePessoas = 3;
+        //boolean acompanhado = quantidadePessoas >= 2;
+		
+		if(idade >= 18 && acompanhado ) {
 			System.out.println("Bem vindo");
 		}
 		else {
