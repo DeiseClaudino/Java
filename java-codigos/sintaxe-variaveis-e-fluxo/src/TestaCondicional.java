e19313b839c929a7e35d97c87df897a1b6a4aacb
Deise Claudino Almeida Pereira <deise.claudino@gmail.com>
2018-05-03 16:07:54 -0300

    Usando condicional composta  no Java (if-else)

--- java-codigos/sintaxe-variaveis-e-fluxo/src/TestaCondicional.java
+++ java-codigos/sintaxe-variaveis-e-fluxo/src/TestaCondicional.java
@@ -5,6 +5,10 @@ public class TestaCondicional {
 		int idade = 20;
 		if(idade >= 18) {
 			System.out.println("Você tem mais de 18 anos!");
+			System.out.println("Bem vindo");
+		}
+		else {
+			System.out.println("Você não pode entrar");
 		}
 	}
 }
