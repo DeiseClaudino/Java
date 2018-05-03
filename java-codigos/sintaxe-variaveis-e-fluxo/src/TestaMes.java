e6dc8f6640ff23022ea43bbb271da0f9029c5ed5
Deise Claudino Almeida Pereira <deise.claudino@gmail.com>
2018-05-03 18:47:03 -0300

    Usando Switch case no Java

---
+++ java-codigos/sintaxe-variaveis-e-fluxo/src/TestaMes.java
@@ -0,0 +1,58 @@
+
+public class TestaMes {
+	public static void main(String[] args) {
+		int mes = 10;
+		switch(mes) {
+		
+		case 1:
+			System.out.println("O mês " + mes +" é Janeiro");
+			break;
+			
+		case 2:
+			System.out.println("O mês " + mes + "  é Fevereiro");
+			break;
+			
+		case 3:
+			System.out.println("O mês "+ mes +"é Março");
+			break;
+			
+		case 4:
+			System.out.println("O mês " + mes +" é Abril");
+			break;
+			
+		case 5:
+			System.out.println("O mês " + mes + " é Maio");
+			break;
+			
+		case 6:
+			System.out.println("O mês " + mes + "é Junho");
+			break;
+			
+		case 7:
+			System.out.println("O mês" + mes + "é Julho");
+			break;
+			
+		case 8:
+			System.out.println("O mês " + mes + " é Agosto");
+			break;
+			
+		case 9:
+			System.out.println("O mês " + mes + "é Setembro");
+			break;
+			
+		case 10:
+			System.out.println("O mês " + mes + " é Outubro");
+			break;
+			
+		case 11:
+			System.out.println("O mês " + mes +"é Novembro");
+			break;
+			
+		case 12:
+			System.out.println("O mês " + mes + " é Dezembro");
+			break;
+		default : 
+			System.out.println("Mês " + mes +" é inválido");
+		}
+	}
+}
