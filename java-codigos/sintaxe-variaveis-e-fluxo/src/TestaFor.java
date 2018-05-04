1cb907f835d03ef8593f8f0d7b4e18b7202b4027
Deise Claudino Almeida Pereira <deise.claudino@gmail.com>
2018-05-04 14:50:21 -0300

    Inicializando variável no laço

--- java-codigos/sintaxe-variaveis-e-fluxo/src/TestaFor.java
+++ java-codigos/sintaxe-variaveis-e-fluxo/src/TestaFor.java
@@ -1,9 +1,8 @@
 
 public class TestaFor {
 	public static void main(String[] args) {
-		int contador = 0;
-		
-		for(contador = 0; contador <= 10; contador++) {
+				
+		for(int contador = 0; contador <= 10; contador++) {
 			System.out.println(contador);
 		}
 	}
