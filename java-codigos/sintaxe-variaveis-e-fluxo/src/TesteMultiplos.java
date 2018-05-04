9e9caf1dc89cf25ab888c3690be677d1e9ef6f3a
Deise Claudino Almeida Pereira <deise.claudino@gmail.com>
2018-05-04 15:53:59 -0300

    Desafio: encontrar os múltiplos de 3 no intervalo de 0 a 100, utilizando if e mod (corrigindo incremento do for)

--- java-codigos/sintaxe-variaveis-e-fluxo/src/TesteMultiplos.java
+++ java-codigos/sintaxe-variaveis-e-fluxo/src/TesteMultiplos.java
@@ -1,7 +1,7 @@
 
 public class TesteMultiplos {
 	public static void main(String[] args) {
-		for(int numero = 0; numero < 100; numero+=3) {
+		for(int numero = 0; numero < 100; numero++) {
 			if(numero %3 == 0) {
 			System.out.println(numero);
 		}
