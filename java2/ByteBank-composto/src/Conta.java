093a30d056628aa30f0a45d7b1b007dc45970c59
Deise Claudino Almeida Pereira <deise.claudino@gmail.com>
2018-05-09 14:57:47 -0300

    Composição de Projetos

---
+++ java2/ByteBank-composto/src/Conta.java
@@ -0,0 +1,38 @@
+public class Conta {
+	double saldo;
+	int agencia =42;
+	int numero;
+	String titular;
+	
+	public void deposita(double valor) {
+		//saldo = saldo + valor
+		this.saldo += valor;
+	}
+	//this é opcional
+	public boolean saca(double valor) {
+		if(this.saldo>= valor) {
+			this.saldo-= valor;
+			return true;
+			
+		}else {
+			System.out.println("Saldo menor que o solicitado no saque");
+			return false;
+		}
+		
+		
+	}
+	
+	//    boolean sucessoTransferencia = contaDaMarcela.transfere(300, contaDoPaulo);
+	public boolean transfere(double valor, Conta destino) {
+		if(this.saldo>= valor) {
+			this.saldo-= valor;
+			destino.deposita(valor);
+			System.out.println("Transferido com sucesso!");
+			return true;	
+			
+		}else {
+			System.out.println("Saldo menor que o solicitado na transferência");
+			return false;
+		}
+	}
+}
