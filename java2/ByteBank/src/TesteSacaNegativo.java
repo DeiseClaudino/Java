568109465043ac691030819574a6ec6bb24882e6
Deise Claudino Almeida Pereira <deise.claudino@gmail.com>
2018-05-09 16:35:25 -0300

    Retornando valor negativo

--- java2/ByteBank/src/TesteSacaNegativo.java
+++ java2/ByteBank/src/TesteSacaNegativo.java
@@ -4,7 +4,10 @@ public class TesteSacaNegativo {
 	public static void main(String[] args) {
 		Conta conta = new Conta();
 		conta.deposita(100);
-		System.out.println(conta.saca(200));
+		System.out.println(conta.saca(101));
+		System.out.println(conta.saldo);
+		
+		conta.saldo -= 101;
 		System.out.println(conta.saldo);
 	}
 }
