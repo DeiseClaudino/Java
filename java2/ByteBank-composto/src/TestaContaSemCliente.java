4de8098441fdf3b8762f62dafe632ec1d3f93b09
Deise Claudino Almeida Pereira <deise.claudino@gmail.com>
2018-05-09 15:47:07 -0300

    A partir do momento que instanciamos a classe Cliente os valores vazios somem e as informações atribuídas às variável são impressas

--- java2/ByteBank-composto/src/TestaContaSemCliente.java
+++ java2/ByteBank-composto/src/TestaContaSemCliente.java
@@ -3,6 +3,9 @@ public class TestaContaSemCliente {
         Conta contaDaMarcela = new Conta();
         System.out.println(contaDaMarcela.saldo); 
 
+        contaDaMarcela.titular = new Cliente(); 
+        System.out.println(contaDaMarcela.titular);
+
         contaDaMarcela.titular.nome = "Marcela";
         System.out.println(contaDaMarcela.titular.nome); 
 
