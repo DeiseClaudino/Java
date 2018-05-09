fbe4668c8484cd5d53422fa6f5677f4e96243497
Deise Claudino Almeida Pereira <deise.claudino@gmail.com>
2018-05-09 18:40:27 -0300

    Get e Set agencia

--- java2/ByteBank-encapsulado/src/Conta.java
+++ java2/ByteBank-encapsulado/src/Conta.java
@@ -47,7 +47,15 @@ public class Conta {
 		return this.numero;
 	}
 	
-	public void setNumero(int novoNumero) {
-		this.numero = novoNumero;
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+	
+	public int getAgencia() {
+		return this.agencia;
+	}
+	
+	public void setAgencia(int agencia) {
+		this.agencia = agencia;
 	}
 }
