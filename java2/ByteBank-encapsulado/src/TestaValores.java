6c6dc692574c765a96dcd59356961c289a1e8bad
Deise Claudino Almeida Pereira <deise.claudino@gmail.com>
2018-05-11 15:59:02 -0300

    Ao criar mais duas contas o contador permanece em um

--- java2/ByteBank-encapsulado/src/TestaValores.java
+++ java2/ByteBank-encapsulado/src/TestaValores.java
@@ -8,5 +8,13 @@ public class TestaValores {
 		System.out.println(conta.getAgencia());
 		
 		conta.setAgencia(1232123);
+		
+		Conta conta2 = new Conta(2334,12354);
+		
+		Conta conta3 = new Conta(8765,43210 );
+		
+		System.out.println(conta2.getAgencia());
+		
+		System.out.println(conta3.getAgencia());
 	}
 }
