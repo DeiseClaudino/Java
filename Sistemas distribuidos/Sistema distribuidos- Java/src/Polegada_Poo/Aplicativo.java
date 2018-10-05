08bb55fe17cdc5b108d34c0c97f2c91acc09acf7
Deise <36971597+DeiseClaudino@users.noreply.github.com>
2018-10-05 09:47:43 -0300

    Add files via upload
    Criando cálculo de milimetros

---
+++ Sistemas distribuidos/Sistema distribuidos- Java/src/Polegada_Poo/Aplicativo.java
@@ -0,0 +1,13 @@
+package Polegada_Poo;
+
+public class Aplicativo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Sistemas distribuidos/Sistema distribuidos- Java/src/Polegada_Poo/Polegada.java
@@ -0,0 +1,13 @@
+package Polegada_Poo;
+
+public class Polegada {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
